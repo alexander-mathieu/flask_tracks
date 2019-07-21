6a4e4d398f036cc534ce03c31895bf7c476f0345
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-21 12:14:51 -0600

    Add initial get_recommended_songs route test for API with parse_response function
    Co-Authored-By: Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, json, jsonify
 import requests
 # import config
@@ -6,6 +7,18 @@ import base64
 
 api = Flask(__name__)
 
+SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+
 @api.route('/', methods = ['GET'])
 def home():
     return '<h1>Welcome to the FastTracks Microservice!</h1>'
+
+@api.route('/api/v1/recommendations', methods = ['GET'])
+def parse_response():
+    ids = request.args.get('song_ids')
+    limit = request.args.get('limit')
+    return get_recommended_songs(ids, limit)
+
+if __name__ == '__main__':
+    api.run()
--- test_api.py
+++ test_api.py
@@ -12,3 +12,9 @@ def client():
 def test_home_page(client):
     r = client.get('/')
     assert b'Welcome to the FastTracks Microservice!' in r.data
+
+def test_get_recommended_songs(client):
+    params = {'song_ids': 'stuff', 'limit': 5}
+
+    r = client.get('/api/v1/recommendations?song_ids=stuff&limit=5')
+    assert b"{ 'spotify_id': 'spotify_id_1', 'title': 'Song 1', 'artist': 'Artist 1', 'album': 'Album 1', 'spotify_url': 'https://spotify.com/1', 'album_art_url': 'https://art.album.com/1', 'length': 11111 }" in r.data
